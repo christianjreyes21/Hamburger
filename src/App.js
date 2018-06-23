da6fe8301c57bf54476949b6f8fcb49f336f6302
Christian Reyes <christianj.reyes21@gmail.com>
2018-06-23 08:42:02 -0700

    Added layout components and a high order component (Aux).

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-
+import Layout from './components/Layout/Layout.js';
 class App extends Component {
   render() {
     return (
       <div >
-
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
