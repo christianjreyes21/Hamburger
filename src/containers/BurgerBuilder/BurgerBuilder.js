2d969913fd68537652de876d3345b1c358a66dfc
Christian Reyes <christianj.reyes21@gmail.com>
2018-06-26 19:06:44 -0700

    Build Controls Added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger.js';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
 
 class BurgerBuilder extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     ...
+  //   }
+  //
+  //}
+  state = {
+    ingredients: {
+      salad: 2,
+      bacon: 4,
+      cheese: 2,
+      meat: 3,
+    }
+  }
   render () {
     return (
       <Aux>
-        <div>Burger</div>
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients}/>
+        <BuildControls />
+
       </Aux>
     );
   }
